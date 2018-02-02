22e1a41f46cb102e87c8c87c4bd0af383a932801
coding-dream <352085768@qq.com>
2018-02-02 23:05:52 +0800

    little update

--- src/main/java/com/less/aspider/test/ProxyProviderTest.java
+++ src/main/java/com/less/aspider/test/ProxyProviderTest.java
@@ -5,7 +5,6 @@ import com.less.aspider.eventbus.BaseEventBus;
 import com.less.aspider.eventbus.SimpleEventBus;
 import com.less.aspider.proxy.ProxyProvider;
 import com.less.aspider.proxy.SimpleProxyProvider;
-import com.less.aspider.samples.JianShuSpider3;
 
 /**
  * Created by deeper on 2018/1/16.
@@ -16,10 +15,6 @@ public class ProxyProviderTest {
     public static void main(String[] args) {
         // testEventBus();
         // testTimerWork();
-    }
-
-    private static void testTimerWork() {
-        JianShuSpider3.timerWork("F:\\jconfig.json", 5, false);
     }
 
     private static void testEventBus() {
